5834d2bf4dfecca2dca231cee8a441b11a31c6cf
MadMad <151575623+0xMadMad@users.noreply.github.com>
2026-08-19 23:38:45 +0330

    Update build.py

--- build.py
+++ build.py
@@ -2,5 +2,5 @@ import os
 
 VERSION = "1.0.0"
 
-os.system(f"pyinstaller --onefile --console --icon=assets/packet-shooter_icon.ico --name=\"Packet Shooter v{VERSION}\" p2pchat_tui.py")
+os.system(f"pyinstaller --onefile --console --icon=assets/packet-shooter_icon.ico --name=\"PacketShooter-v{VERSION}\" p2pchat_tui.py")
 # _6
